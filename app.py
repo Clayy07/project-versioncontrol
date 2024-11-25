f968eaf5b9bef5bf66fec022352445ac920f4459
Clay Justin <clywwr@gmail.com>
2024-11-25 23:54:48 +0800

    Initial commit: Menambah app.py

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
+import subprocess
+
+def build_docker_image():
+    subprocess.run(['docker', 'build', '-t', 'flask_app', '.'])
+
+def run_docker_container():
+    subprocess.run(['docker', 'run', '-d', '-p', '5000:5000', '--name clay_app', 'flask_app'])
+
+if __name__ == "__main__":
+    build_docker_image()
+    run_docker_container()
+    print("Aplikasi berjalan di http://localhost:5000")
